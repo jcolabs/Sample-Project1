8aa6cc35ae92ebc24b079d1928843abf9cf90fee
jcolabs <jn6220c.g@gmail.com>
2021-09-16 17:45:41 +0800

    Navbar and logo changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,36 @@
-import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import logo from "../logo.png";
+import { faAddressBook, faBackward, faBars, faChartLine, faHistory, faHome } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">Navbar</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mr-auto">
-                <li className="nav-item active">
-                    <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Link</a>
-                </li>
-                <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
-                    </a>
-                    <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a className="dropdown-item" href="#">Action</a>
-                    <a className="dropdown-item" href="#">Another action</a>
-                    <div className="dropdown-divider"></div>
-                    <a className="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link disabled" href="#">Disabled</a>
-                </li>
-                </ul>
+        <nav className="navbar navbar-expand-lg navbar-light" style={{backgroundColor: "grey"}}>
+            <div className="container">
+                <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="..."></img></a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <FontAwesomeIcon icon={faBars} style={{ color: "#fff" }} />
+                </button>
+                
+                <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item">
+                            <a className="nav-link" href="?pg=home"><FontAwesomeIcon icon={faHome} /> <small>Home</small></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="?pg=link"><FontAwesomeIcon icon={faAddressBook} /> <small>Link</small></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="?pg=refs"><FontAwesomeIcon icon={faBackward} /> <small>References</small></a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="?pg=abt" className="nav-link"><FontAwesomeIcon icon={faHistory} /> <small>About</small></a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="?pg=org" className="nav-link"><FontAwesomeIcon icon={faChartLine} /> <small>Organization</small></a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     )
